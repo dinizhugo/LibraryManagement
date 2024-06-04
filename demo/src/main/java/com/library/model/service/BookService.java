5d4be002e5ea4e6a60d795f1e40ec84c8b7a1ec1
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-06-03 23:54:55 -0300

    Create BookService

---
+++ demo/src/main/java/com/library/model/service/BookService.java
@@ -0,0 +1,39 @@
+package com.library.model.service;
+
+import com.library.model.dao.DaoFactory;
+import com.library.model.dao.GenericDao;
+import com.library.model.entities.Book;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class BookService {
+    private GenericDao<Book> bookDao = DaoFactory.createBookDao();
+
+    public void addBook(String title, String author, String publishingCompany, String category, int amount, LocalDate publicationDate) {
+        bookDao.insert(new Book(null, title, author, publishingCompany, category, amount,publicationDate));
+    }
+
+    public void updateBook(Book book, String title, String author, String publishingCompany, String category, int amount, LocalDate publicationDate) {
+        book.setTitle(title);
+        book.setAuthor(author);
+        book.setPublishingCompany(publishingCompany);
+        book.setCategory(category);
+        book.setAmount(amount);
+        book.setPublicationDate(publicationDate);
+        bookDao.update(book);
+    }
+
+    public void deleteBookById(Integer id) {
+        bookDao.deleteById(id);
+    }
+
+    public Book getBookById(Integer id) {
+        return bookDao.findById(id);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookDao.findAll();
+    }
+
+}
